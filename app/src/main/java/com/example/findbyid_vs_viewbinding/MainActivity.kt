588f7f5a039aa6711b4ae1fb96e53499ae7e1db6
miyaji555 <kou.sepak@gmail.com>
2022-04-03 17:56:03 +0900

    buttonを押したらtextが変更されるようにできました。

--- app/src/main/java/com/example/findbyid_vs_viewbinding/MainActivity.kt
+++ app/src/main/java/com/example/findbyid_vs_viewbinding/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.findbyid_vs_viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.textView)
+        rollButton.setOnClickListener {
+            textView.text = "changed"
+        }
     }
 }
